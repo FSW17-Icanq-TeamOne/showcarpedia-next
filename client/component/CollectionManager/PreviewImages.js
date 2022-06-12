ce3267ea7d9abd10519560b2137f438af8d4e5fb
kelvin handoko <kelvinhandoko03@gmail.com>
2022-06-12 09:33:11 +0700

    feat:add video form in create product

--- client/component/CollectionManager/PreviewImages.js
+++ client/component/CollectionManager/PreviewImages.js
@@ -1,94 +1,88 @@
 import {
-  Box,
-  Button,
   ImageList,
   ImageListItem,
   ImageListItemBar,
   Typography,
   IconButton,
-} from "@mui/material";
-import { Clear } from "@mui/icons-material";
-import React, { useEffect, useState } from "react";
-import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
-import { storage } from "../../config/firebase";
-import { LoadingButton } from "@mui/lab";
-import { Card, CardActionArea } from "@mui/material";
-import { useRef } from "react";
-import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate";
-import { CardActions } from "@mui/material";
-import { Send } from "@mui/icons-material";
-import { LinearProgress } from "@mui/material";
-import { CardContent } from "@mui/material";
-import { Grid } from "@mui/material";
+} from "@mui/material"
+import {Clear} from "@mui/icons-material"
+import React, {useEffect, useState} from "react"
+import {getDownloadURL, ref, uploadBytesResumable} from "firebase/storage"
+import {storage} from "../../config/firebase"
+import {LoadingButton} from "@mui/lab"
+import {Card, CardActionArea} from "@mui/material"
+import {useRef} from "react"
+import AddPhotoAlternateIcon from "@mui/icons-material/AddPhotoAlternate"
+import {CardActions} from "@mui/material"
+import {Send} from "@mui/icons-material"
+import {LinearProgress} from "@mui/material"
+import {CardContent} from "@mui/material"
+import {Grid} from "@mui/material"
 
 export default function PreviewImages(props) {
-  const {data,images} = props
-  const [files, setFiles] = useState([]);
-  const [progress, setProgress] = useState(0);
-  const [loading, setLoading] = useState(false);
-  const inputRef = useRef(null);
+  const {data, images} = props
+  const [files, setFiles] = useState([])
+  const [progress, setProgress] = useState(0)
+  const [loading, setLoading] = useState(false)
+  const inputRef = useRef(null)
 
   const handleInput = () => {
-    inputRef.current.click();
-  };
+    inputRef.current.click()
+  }
 
   useEffect(() => {
     if (images) {
-      setFiles([...images]);
+      setFiles([...images])
     }
-  }, [images]);
+  }, [images])
 
   useEffect(() => {
     files.map((file) => {
-      return () => URL.revokeObjectURL(file);
-    });
-  }, [files]);
+      return () => URL.revokeObjectURL(file)
+    })
+  }, [files])
 
   const handleChange = (e) => {
     const files = Array.from(e.target.files)
     files.forEach((file) => {
-      file.preview = URL.createObjectURL(file);
-      setFiles((prev) => [...prev, file]);
-    });
-    e.target.value= ""
-  };
+      file.preview = URL.createObjectURL(file)
+      setFiles((prev) => [...prev, file])
+    })
+    e.target.value = ""
+  }
 
   const handleDelete = (index) => {
-    setFiles((files) => files.filter((_, idx) => idx !== index));
-  };
+    setFiles((files) => files.filter((_, idx) => idx !== index))
+  }
 
   const handleUpload = async () => {
-    
-    const promises= files.map(async (file) => {
-     const storageRef = ref(storage, `images/${file.name}`);
-      const uploadTask = uploadBytesResumable(storageRef,file)
-      console.log(uploadTask)
+    const promises = files.map(async (file) => {
+      const storageRef = ref(storage, `images/${file.name}`)
+      const uploadTask = uploadBytesResumable(storageRef, file)
       uploadTask.on(
-       `state_changed`,
-       snapshot => {
-           const prog = 
-            (snapshot.bytesTransferred / snapshot.totalBytes) * 100
-           
-           console.log(prog)
-           setProgress(prog)
-           setLoading(true)
-       },
-       error => {
-           console.log(error)
-       }
+        `state_changed`,
+        (snapshot) => {
+          const prog = (snapshot.bytesTransferred / snapshot.totalBytes) * 100
+
+          console.log(prog)
+          setProgress(prog)
+          setLoading(true)
+        },
+        (error) => {
+          console.log(error)
+        }
       )
-     return uploadTask.then(snapshot => getDownloadURL(snapshot.ref)) 
-   });
-   
-   try {
-     const urls = await Promise.all(promises)
-     setLoading(false)
-     data(urls);
-   } catch (error) {
-     console.log(error);
-   }
- };
- console.log(files)
+      return uploadTask.then((snapshot) => getDownloadURL(snapshot.ref))
+    })
+
+    try {
+      const urls = await Promise.all(promises)
+      setLoading(false)
+      data(urls)
+    } catch (error) {
+      console.log(error)
+    }
+  }
   return (
     <>
       <Card
@@ -97,11 +91,12 @@ export default function PreviewImages(props) {
           height: "100%",
           maxWidth: "540px",
           minHeight: "396px",
+          maxHeight: "720px",
           padding: 1,
         }}
       >
         {!files?.length ? (
-          <CardActionArea onClick={handleInput} sx={{ height: "100%" }}>
+          <CardActionArea onClick={handleInput} sx={{height: "100%"}}>
             <CardContent
               sx={{
                 display: "flex",
@@ -111,10 +106,7 @@ export default function PreviewImages(props) {
               }}
             >
               <AddPhotoAlternateIcon fontSize="large" />
-              <Typography
-                variant="h3"
-                style={{ fontSize: "calc(1rem + .5vw)" }}
-              >
+              <Typography variant="h3" style={{fontSize: "calc(1rem + .5vw)"}}>
                 click here to add images
               </Typography>
             </CardContent>
@@ -123,11 +115,20 @@ export default function PreviewImages(props) {
           <>
             <Grid container>
               <Grid item xs={12} height="90%">
-                <CardActionArea onClick={handleInput} sx={{ height: "100%" }}>
-                  <ImageList sx={{ height:480,paddingBottom:3}} rowHeight={180} cols={3}>
+                <CardActionArea onClick={handleInput} sx={{height: "100%"}}>
+                  <ImageList
+                    sx={{height: 480, paddingBottom: 3}}
+                    rowHeight={180}
+                    cols={3}
+                  >
                     {files?.map((file, idx) => (
                       <ImageListItem key={idx}>
-                        <img src={file.preview??file} alt={file} loading="lazy" height={"100%"} />
+                        <img
+                          src={file.preview ?? file}
+                          alt={file}
+                          loading="lazy"
+                          height={"100%"}
+                        />
                         <ImageListItemBar
                           sx={{
                             background:
@@ -137,10 +138,10 @@ export default function PreviewImages(props) {
                           position="top"
                           actionIcon={
                             <IconButton
-                              sx={{ color: "white" }}
+                              sx={{color: "white"}}
                               onClick={(e) => {
-                                e.stopPropagation();
-                                handleDelete(idx);
+                                e.stopPropagation()
+                                handleDelete(idx)
                               }}
                             >
                               <Clear />
@@ -154,7 +155,7 @@ export default function PreviewImages(props) {
                 </CardActionArea>
               </Grid>
               <Grid item xs={12}>
-              <LinearProgress variant="determinate" value={progress} />
+                <LinearProgress variant="determinate" value={progress} />
                 <CardActions onClick={handleUpload}>
                   <LoadingButton
                     loading={loading}
@@ -176,10 +177,10 @@ export default function PreviewImages(props) {
       <input
         type="file"
         multiple
-        style={{ display: "none" }}
+        style={{display: "none"}}
         ref={inputRef}
         onChange={handleChange}
       />
     </>
-  );
+  )
 }
