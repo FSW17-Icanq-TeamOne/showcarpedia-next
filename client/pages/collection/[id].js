2b44cb49ce151297258bf3053c81cf9c91a1ddc7
kelvin handoko <kelvinhandoko03@gmail.com>
2022-06-14 10:43:16 +0700

    fix bug

--- client/pages/collection/[id].js
+++ client/pages/collection/[id].js
@@ -2,17 +2,12 @@ import MainNavbar from "../../component/NavBar/MainNavbar"
 import {Grid} from "@mui/material"
 import Details from "../../component/CollectionPage/CardDetail"
 import {useRouter} from "next/router"
-import {useDispatch, useSelector} from "react-redux"
+import {useSelector} from "react-redux"
 import {useEffect} from "react"
-import {fetchCollectionDataById} from "../../redux/slices/collectionsSlice"
 
 export default function DetailProduct() {
   const data = useSelector((state) => state.collections)
   const router = useRouter()
-  const {id} = router.query
-  const loadData = async () => {
-    await dispatch(fetchCollectionDataById(id))
-  }
 
   useEffect(() => {
     if (!router.isReady) return
