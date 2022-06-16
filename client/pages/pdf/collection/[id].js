129e5e1b5f3c501552bc3fb22b7733763c508f2f
jepra777 <jere.prasetyo.jp@gmail.com>
2022-06-16 11:56:20 +0700

    Fix jsPDF features

--- client/pages/pdf/collection/[id].js
+++ client/pages/pdf/collection/[id].js
@@ -24,62 +24,99 @@ export default function CollectionPDF() {
     },[router.isReady])
 
     useEffect(() => {
-        if(data.data?.title == null) return
-        if (data.data?.title != null){
-        pdfGenerate()
-        Router.push("/admin/collection/lists")
+        if(data.data[0]?.title == null) return
+        if(data.data[0]?.title != null){
+           console.log(data.data[0]?.title)
+           pdfGenerate()
+           Router.push("/admin/collection/lists")
         }
     },[data])
 
-    const getBase64Image = (url) => {
-        const img = new Image();
-        img.setAttribute('crossOrigin', '');
-        img.onload = () => {
-          const canvas = document.createElement("canvas");
-          canvas.width = img.width;
-          canvas.height = img.height;
-          const ctx = canvas.getContext("2d");
-          ctx.drawImage(img, 0, 0);
-          const dataURL = canvas.toDataURL("image/png");
-          console.log(dataURL)
-        }
-        img.src = url
-      }
-
     const pdfGenerate = () => {
-        let doc = new jsPDF('portrait', 'px', 'a4', 'false')
-        // let urlFoto = "https://i.stack.imgur.com/EoOaz.png"
-        // let fotoUrl = getBase64Image(urlFoto)
-        // console.log("ini url", fotoUrl)
-        let photo = data.data?.photoProducts
-
-        let counter = 0
-        photo?.map((e, i) => {
-            let photoProduct = new Image()
-            photoProduct.src = e
-            photoProduct.alt = "alt"
-            doc.addImage(photoProduct, "PNG", 65, 20+counter, 150, 150)
-            counter=counter+200
+        let doc = new jsPDF({
+            orientation: 'potrait',
+            unit: 'px',
+            format: 'a4'
         })
-        doc.addPage()
-        doc.text(60, 60, "Title")
-        doc.text(60, 80, "Brand")
-        doc.text(60, 100, "Grade")
-        doc.text(60, 120, "Category")
-        doc.text(60, 140, "Year")
-        doc.text(60, 160, "Kilo Meter")
-        doc.text(60, 180, "Description")
-        doc.text(140, 60, `: ${data.data?.title}`)
-        doc.text(140, 80, `: ${data.data?.brand}`)
-        doc.text(140, 100, `: ${data.data?.grade}`)
-        doc.text(140, 120, `: ${data.data?.category}`)
-        doc.text(140, 140, `: ${data.data?.year}`)
-        doc.text(140, 160, `: ${data.data?.kiloMeter}`)
-        doc.text(140, 180, `: ${data.data?.description}`)
-        doc.save('collection.pdf')
+       // Header
+       doc.rect(100, 50, 250, 10)
+       doc.setFontSize(9)
+       doc.text('Showcarpedia Collection Report (2022)', 110, 57.5)
+
+       // Main Bar
+       doc.rect(100, 60, 250, 500)
+
+       doc.setFontSize(20)
+       doc.text(`${data.data[0]?.title}`, 110, 85)
+
+       // Fact Box
+       doc.rect(110, 90, 230, 105)
+
+       doc.setFontSize(10)
+       // Brand
+       doc.text(`Brand`, 120, 105)
+       doc.text(`: ${data.data[0]?.brand}`, 180, 105)
+       // Grade
+       doc.text(`Grade`, 120, 120)
+       doc.text(`: ${data.data[0]?.grade}`, 180, 120)
+       // Category
+       doc.text(`Category`, 120, 135)
+       doc.text(`: ${data.data[0]?.category}`, 180, 135)
+       // Year
+       doc.text(`Year`, 120, 150)
+       doc.text(`: ${data.data[0]?.year}`, 180, 150)
+       // Kilometer
+       doc.text(`Kilometer`, 120, 165)
+       doc.text(`: ${data.data[0]?.kiloMeter} KM`, 180, 165)
+       // Description
+       doc.text(`Description`, 120, 180)
+       doc.text(`: ${data.data[0]?.description} KM`, 180, 180)
+
+       // Photo Box
+       doc.rect(110, 235, 230, 300)
+       doc.setFontSize(20)
+       doc.text(`Photo Collection`, 110, 230)
+
+       // Photos
+       let photo = data.data[0]?.photoProducts
+
+       let xcounter = 0;
+       
+       // for (let index = 0; index < array.length; index++) {
+       //     const element = array[index];
+           
+       // }
+
+       // if (photo.length <= 3) {
+           photo?.map((e, i) => {
+               let photoProduct = new Image()
+               photoProduct.src = e
+               photoProduct.alt = "alt"
+               if (i <= 2) {
+                   doc.addImage(photoProduct, "PNG", 120+xcounter, 245, 60, 60)
+                   xcounter+=75;
+               } else if (3 <= i <= 5) {
+                   doc.addImage(photoProduct, "PNG", -505+xcounter, 315, 60, 60)
+                   xcounter+=75;
+               } 
+               // else if (6 <= i <= 8) {
+               //     doc.addImage(photoProduct, "PNG", -105+xcounter, 400, 60, 60)
+               //     xcounter+=75;
+               // } else if (9 <= i <= 11) {
+               //     doc.addImage(photoProduct, "PNG", -105+xcounter, 500, 60, 60)
+               //     xcounter+=75;
+               // }
+           })
+       // }
+
+       // Footer
+       doc.rect(100, 550, 250, 10)
+       doc.setFontSize(9)
+       doc.text('Showcarpedia Collection Report (2022)', 222.5, 557.5)
+        doc.save(`collection-${data.data[0]?.title}.pdf`)
     }
     return(
         <>
         </>
     )
-}
+} 
